80d96743bccdb7832d23a733f2c6491683c30286
Jan-Kristian Evjen <jan-kristian.evjen@nrk.no>
2023-10-03 12:48:35 +0200

    Initial Commit

---
+++ WeatherVibes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherVibes
+//
+//  Created by Jan-Kristian Evjen on 03/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherVibes/WeatherVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherVibesApp.swift
+//  WeatherVibes
+//
+//  Created by Jan-Kristian Evjen on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
